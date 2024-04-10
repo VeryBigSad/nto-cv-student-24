fcfbdfcaa99b25ae94a7428bdafb4c3fd4cee45b
Mikhail Khromov <khromov05@gmail.com>
2024-04-10 18:08:48 +0300

    fix typo

--- app/core/routes/v1/classify_text.py
+++ app/core/routes/v1/classify_text.py
@@ -1,12 +1,12 @@
 import logging
-from fastapi import APIRouter
-from aiogram import Bot
-from core.schemas.v1.schemas import ClassifyText
-from core.schemas.v1.enums import CityEnum
-from core.wlui.context import WLUIContextVar
-from configs.settings import env_parameters
-from fastapi import status
+
 import aiohttp
+from aiogram import Bot
+from configs.settings import env_parameters
+from core.schemas.v1.enums import CityEnum
+from core.schemas.v1.schemas import ClassifyText
+from core.wlui.context import WLUIContextVar
+from fastapi import APIRouter, status
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -25,10 +25,10 @@ async def classify_text_route(body: ClassifyText.Request, city: CityEnum):
     city_letter = city.get_letter()
     async with aiohttp.ClientSession() as session:
         async with session.post(
-            f"{env_parameters.API_URL}/text?city={city_letter}",
-            json={"text": text},
+            f"{env_parameters.API_URL}/text",
+            json={"text": text, "city": city_letter},
         ) as response:
-            response = await response.json()
+            response_json = await response.json()
     results = [{
         "xid": "test_xid",
         "name": res.get("name"),
@@ -36,5 +36,5 @@ async def classify_text_route(body: ClassifyText.Request, city: CityEnum):
         "city": city,
         "probs": res.get("probs"),
         "coordinates": res.get("coord"),
-    } for res in response]
+    } for res in response_json]
     return results
